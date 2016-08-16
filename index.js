e7fd718ac938c71ec2cfee51f545ddf12268409b
daniel-j <daniel.j.hede@gmail.com>
2016-08-16 16:30:34 +0200

    cache

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const send = require('koa-send')
 const favicon = require('koa-favicon')
 const app = koa()
 const config = require(__dirname + '/config.json')
+const thenify = require('thenify')
+const fs = require('fs')
+
+const fsreadFile = thenify(fs.readFile)
+const fswriteFile = thenify(fs.writeFile)
 
 app.name = 'fimfic2epub-server'
 app.port = config.port
@@ -23,14 +28,57 @@ app.use(route.get('/', function *() {
 }))
 
 function *handleDownload (id) {
-  const ffc = new FimFic2Epub(id)
+  id = parseInt(id, 10)
+  if (isNaN(id) || id === 0) {
+    return
+  }
 
-  yield ffc.download()
+  let useCache = true
+  let storyInfo, cachedInfo
+  let infoFile = 'archive/' + id + '.json'
+  let storyFile = 'archive/' + id + '.epub'
 
-  let file = yield ffc.getFile()
+  try {
+    cachedInfo = JSON.parse(yield fsreadFile(infoFile))
+  } catch (err) { }
+  try {
+    storyInfo = yield FimFic2Epub.fetchStoryInfo(id)
+  } catch (err) { }
+
+  if (storyInfo && !cachedInfo) {
+    useCache = false
+  } else if (storyInfo && cachedInfo) {
+    if (JSON.stringify(storyInfo) !== JSON.stringify(cachedInfo)) {
+      useCache = false
+    }
+  } else if (!storyInfo && !cachedInfo) {
+    return
+  }
+
+  storyInfo = storyInfo || cachedInfo
+
+  let file, filename
+
+  if (useCache) {
+    file = yield fsreadFile(storyFile)
+    filename = FimFic2Epub.getFilename(storyInfo)
+    console.log('Serving cached ' + filename)
+  } else {
+    const ffc = new FimFic2Epub(id)
+
+    yield ffc.download()
+
+    file = yield ffc.getFile()
+    filename = ffc.filename
+
+    console.log('Serving generated ' + filename)
+
+    fswriteFile(storyFile, file)
+    fswriteFile(infoFile, JSON.stringify(storyInfo))
+  }
 
   this.response.type = 'application/epub+zip'
-  this.response.attachment(ffc.filename)
+  this.response.attachment(filename)
 
   this.response.body = file
 }
