b607829b0370047118f88d85dc2dba45a11d4eff
daniel-j <daniel.j.hede@gmail.com>
2016-08-29 11:47:10 +0200

    sync version

--- server/generateEpub.js
+++ server/generateEpub.js
@@ -4,9 +4,16 @@ const FimFic2Epub = require('fimfic2epub')
 
 function generateEpub (id) {
   const ffc = new FimFic2Epub(id)
-  return ffc.download().then(ffc.getFile.bind(ffc)).then((file) => {
-    return {file: file, filename: ffc.filename}
+  ffc.on('progress', (percent, status) => {
+    if (status) {
+      console.log(status)
+    }
   })
+  return ffc.fetchAll()
+    .then(ffc.build.bind(ffc))
+    .then(ffc.getFile.bind(ffc)).then((file) => {
+      return {file: file, filename: ffc.filename}
+    })
 }
 
 module.exports = generateEpub
